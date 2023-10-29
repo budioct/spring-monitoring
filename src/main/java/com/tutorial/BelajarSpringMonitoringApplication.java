d2df3158a79fbc261c1fd7a8e72ac99a273eeb49
Budhi Octaviansyah <budioct@gmail.com>
2023-10-29 12:33:04 +0700

    note untuk tidak expose endpoint actuator/ monitoring

--- src/main/java/com/tutorial/BelajarSpringMonitoringApplication.java
+++ src/main/java/com/tutorial/BelajarSpringMonitoringApplication.java
@@ -8,6 +8,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class BelajarSpringMonitoringApplication {
 
+	/**
+	 * note:
+	 * Peringatan
+	 * ● Perlu diperhatikan bahwa fitur Actuator sangat sensitif karena berisi informasi yang mungkin
+	 *   rahasia, sehingga tidak boleh diakses secara public oleh semua orang
+	 * ● Oleh karena itu, sangat disarankan untuk tidak mengekspos endpoint actuator ke public
+	 * ● Kita bisa tambahkan Firewall atau Proxy Server seperti Nginx untuk menjaga agar endpoint
+	 *   Actuator tidak bisa diakses secara bebas oleh public
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(BelajarSpringMonitoringApplication.class, args);
 	}
